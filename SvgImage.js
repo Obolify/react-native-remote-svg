4c0c8285a220da7b5b9e7d70667f9a2f60df4fbd
Magnus Singer <magnussinger@icloud.com>
2022-08-23 16:45:20 +0200

    fix styling for our needs

--- SvgImage.js
+++ SvgImage.js
@@ -1,104 +1,112 @@
 // @flow
 
-import React, { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
-import { WebView } from "react-native-webview";
-
-const heightUnits = Platform.OS === "ios" ? "vh" : "%";
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const getHTML = (svgContent, style) => `
 <html data-key="key-${style.height}-${style.width}">
   <head>
     <style>
-      html, body {
+      * {
         margin: 0;
         padding: 0;
-        height: 100${heightUnits};
-        width: 100${heightUnits};
-        overflow: hidden;
-        background-color: transparent;
+        box-sizing: border-box;
+      }
+      #container {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: ${style.backgroundColor};
+        border-radius: ${style.borderRadius}px;
       }
       svg {
-        position: fixed;
-        top: 0;
-        left: 0;
         height: 100%;
         width: 100%;
-        overflow: hidden;
+        background: ${style.backgroundColor};
+        border-radius: ${style.borderRadius}px;
       }
     </style>
   </head>
   <body>
-    ${svgContent}
+    <div id="container">
+        ${svgContent}
+    </div>
   </body>
 </html>
 `;
 
-function SvgImage({ source, onLoadStart, onLoadEnd, style, containerStyle }) {
-  const [svgContent, setSvgContent] = useState(null);
+class SvgImage extends Component {
+  state = { fetchingUrl: null, svgContent: null };
+  componentDidMount() {
+    this.doFetch(this.props);
+  }
+  componentWillReceiveProps(nextProps) {
+    const prevUri = this.props.source && this.props.source.uri;
+    const nextUri = nextProps.source && nextProps.source.uri;
 
-  const uri = source && source.uri;
-  useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-
-    async function doFetch() {
-      if (uri) {
-        onLoadStart && onLoadStart();
-        if (uri.match(/^data:image\/svg/)) {
-          const index = uri.indexOf("<svg");
-          setSvgContent(uri.slice(index));
-        } else {
-          try {
-            const res = await fetch(uri, { signal });
-            const text = await res.text();
-            setSvgContent(text);
-          } catch (err) {
-            console.error("got error", err);
-          }
-        }
-        onLoadEnd && onLoadEnd();
-      }
+    if (nextUri && prevUri !== nextUri) {
+      this.doFetch(nextProps);
     }
+  }
+  doFetch = async props => {
+    let uri = props.source && props.source.uri;
+    if (uri) {
+      props.onLoadStart && props.onLoadStart();
+      if (uri.match(/^data:image\/svg/)) {
+        const index = uri.indexOf('<svg');
+        this.setState({ fetchingUrl: uri, svgContent: uri.slice(index) });
+      } else {
+        try {
+          const res = await fetch(uri);
+          const text = await res.text();
+          this.setState({ fetchingUrl: uri, svgContent: text });
+        } catch (err) {
+          console.error('got error', err);
+        }
+      }
+      props.onLoadEnd && props.onLoadEnd();
+    }
+  };
+  render() {
+    const props = this.props;
+    const { svgContent } = this.state;
+    if (svgContent) {
+      const flattenedStyle = StyleSheet.flatten(props.style) || {};
+      const html = getHTML(svgContent, flattenedStyle);
 
-    doFetch();
-
-    return () => {
-      controller.abort();
-    };
-  }, [uri]);
-
-  if (svgContent) {
-    const flattenedStyle = StyleSheet.flatten(style) || {};
-    const html = getHTML(svgContent, flattenedStyle);
-
-    return (
-      <View
-        pointerEvents="none"
-        style={[style, containerStyle]}
-        renderToHardwareTextureAndroid={true}
-      >
-        <WebView
-          originWhitelist={["*"]}
-          scalesPageToFit={true}
-          useWebKit={false}
-          style={[
-            {
-              width: 200,
-              height: 100,
-              backgroundColor: "transparent",
-            },
-            style,
-          ]}
-          scrollEnabled={false}
-          showsHorizontalScrollIndicator={false}
-          showsVerticalScrollIndicator={false}
-          source={{ html }}
+      return (
+        <View pointerEvents="none" style={[props.style, props.containerStyle]}>
+          <WebView
+            originWhitelist={['*']}
+            scalesPageToFit={false}
+            useWebKit={false}
+            style={[
+              {
+                width: 200,
+                height: 200,
+                backgroundColor: 'transparent'
+              },
+              props.style,
+            ]}
+            scrollEnabled={false}
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
+            androidLayerType='software'
+            source={{ html }}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View
+          pointerEvents="none"
+          style={[props.containerStyle, props.style]}
         />
-      </View>
-    );
-  } else {
-    return <View pointerEvents="none" style={[containerStyle, style]} />;
+      );
+    }
   }
 }
 
